ac0b947c93987d6e289e2a975419ba8f4cf115ba
xiezhenxiang <1430742291@qq.com>
2026-08-13 11:35:44 +0800

    --

--- src/main/java/indi/shine/stock/env/EnvConfig.java
+++ src/main/java/indi/shine/stock/env/EnvConfig.java
@@ -25,7 +25,7 @@ public class EnvConfig {
 
     public static final Random RANDOM = new Random();
 
-    private static final String DAY_K_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?secid={type}.{code}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&klt=101&fqt=1&beg=0&end=20500101&smplmt=460&lmt={days}&_=1785489905846";
+    private static final String DAY_K_URL = "https://push2his.eastmoney.com/api/qt/stock/kline/get?secid={type}.{code}&ut=fa5fd1943c7b386f172d6893dbfba10b&fields1=f1,f2,f3,f4,f5,f6&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61&dect=1&klt=101&lmt={days}&fqt=1&forcect=1&end=20500000&wbp2u=1849325530509956|0|1|0|web";
 
     private static final String MINUTE_K_URL = "https://4.push2.eastmoney.com/api/qt/stock/trends2/sse?fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f17&fields2=f51,f52,f53,f54,f55,f56,f57,f58&mpi=1000&ut=fa5fd1943c7b386f172d6893dbfba10b&" +
             "secid=%s.%s&ndays=1&iscr=0&iscca=0&wbp2u=8888326347002756|0|1|0|web";
